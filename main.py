ca85bf519008208e1cafe464ac2faf8758728c48
MartinFlischman <martincodes@icloud.com>
2024-07-08 14:06:48 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+greeting = "Welcome to the Band Name Generator!\n"
+print(greeting)
+
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+band_name = city + " " + pet_name
+print("Your band name could be " + band_name + "! 🎸")
